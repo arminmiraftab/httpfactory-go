b02e1997b75128bd37c86dba100e4590504cf3f1
arminmiraftab <miraftab.armin@gmail.com>
2025-07-19 14:29:47 +0330

    fix roundTripperFunc

---
+++ httpfactory/middleware/common.go
@@ -0,0 +1,10 @@
+package middleware
+
+import "net/http"
+
+// roundTripperFunc wraps a function so it implements http.RoundTripper
+type roundTripperFunc func(*http.Request) (*http.Response, error)
+
+func (f roundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req)
+}
